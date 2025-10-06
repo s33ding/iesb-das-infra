0970079d8ce8d8eddc8ef483a493bfbd69060b14
robertomdiniz <roberto.diniz@tisaude.com>
2025-10-06 10:59:05 -0300

    Update DNS configuration and IDE ingress

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,6 +1,6 @@
-# Route 53 Hosted Zone
-resource "aws_route53_zone" "ads_zone" {
-  name = "dataiesb.com"
+# Use existing hosted zone (the original one by ID)
+data "aws_route53_zone" "dataiesb_zone" {
+  zone_id = "Z05014761ROYBA3Z5YKY2"
 }
 
 # Data source to get the current ALB created by ingress
@@ -10,7 +10,7 @@ data "aws_lb" "ide_alb" {
 
 # A record pointing to ALB
 resource "aws_route53_record" "ads_record" {
-  zone_id = aws_route53_zone.ads_zone.zone_id
+  zone_id = data.aws_route53_zone.dataiesb_zone.zone_id
   name    = "ads.dataiesb.com"
   type    = "A"
 
@@ -19,9 +19,4 @@ resource "aws_route53_record" "ads_record" {
     zone_id                = data.aws_lb.ide_alb.zone_id
     evaluate_target_health = true
   }
-}
-
-# Outputs
-output "nameservers" {
-  value = aws_route53_zone.ads_zone.name_servers
 }
